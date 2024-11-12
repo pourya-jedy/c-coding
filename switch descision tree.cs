ca7db27f99eae7eb814bb0b94475e8052ed6b8fc
pourya-jedy <pourya.2005.official@gmail.com>
2024-11-12 10:25:40 -0800

    Add files via upload

---
+++ switch descision tree.cs
@@ -0,0 +1,34 @@
+﻿int foodname = 1;
+switch (foodname)
+{
+    case 1:
+        Console.WriteLine("pizza");
+        if (foodname == 1)
+            Console.WriteLine("suggest our drinks");
+        else if (foodname != 1)
+            Console.WriteLine("refer customer to next foods");
+
+        break;
+    case 2:
+        Console.WriteLine("lazana");
+        if (foodname == 2)
+            Console.WriteLine("suggest our salad");
+        else if (foodname != 2)
+            Console.WriteLine("refer customer to next foods");
+        break;
+    case 3:
+        Console.WriteLine("Pastais is not availavble");
+        if (foodname == 3)
+            Console.WriteLine("suggest our dessert");
+        else if (foodname != 3)
+            Console.WriteLine("refer customer to next foods");
+        break;
+    case 4:
+        Console.WriteLine("traditional foods are prepared");
+        if (foodname == 4)
+            Console.WriteLine("suggest our appetizers");
+        else if (foodname != 4)
+            Console.WriteLine("refer customer to next foods");
+        break;
+}
+
